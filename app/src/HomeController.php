f1ca4f31434162384a0a5fdedaec88d0d6f16823
“Romangit config --global user.email roman.paliy@ekreative.comgit config --global user.name “Roman <roman.paliy@ekreative.com>
2015-10-16 11:04:31 +0300

    add method getUser and ext-curl in composer

--- app/src/HomeController.php
+++ app/src/HomeController.php
@@ -21,35 +21,9 @@ class HomeController
                 'username' => $_POST['login'],
                 'password' => $_POST['password']
             ]]);
-            if ($result->getStatusCode() === 200) {
-                $get_user = new GuzzleHttp\Client();
-                $secret = 'WEFEpcGFWIq!ya7au-t-Krt_JnJKD6_o9K*l';
-                $key = 'L*D3iK5bMWk9hYRirt_h!pP2';
-                $codice_sha1 = sha1(implode(',', ['userid' => trim($_POST['login']), 'also_check_as_email' => true]) . ',' . $secret);
-                $codice = base64_encode($key . ':' . $codice_sha1);
-                $user = $get_user->post('http://geekhub.docebosaas.com/api/user/checkUsername', [
-                    'form_params' => ['userid' => $_POST['login'], 'also_check_as_email' => true],
-                    'headers' => [
-                        'X-Authorization' => $codice
-                    ]
-                ]);
-                $json = json_decode($user->getBody()->getContents());
-                $_SESSION['docebo_name'] = $json->firstname;
-                $_SESSION['docebo_last'] = $json->lastname;
-                $_SESSION['docebo_email'] = $json->email;
-                $template = new TemplateLoader();
-                $render = $template->getTemplate('login.html');
-                $today = \Carbon\Carbon::now();
-                echo $render->render([
-                    'login' => true,
-                    'firstname' => $_SESSION['docebo_name'],
-                    'lastname' => $_SESSION['docebo_last'],
-                    'email' => $_SESSION['docebo_email'],
-                    'today' => $today
-                ]);
-            } else {
-                header("Location: /home");
-            }
+
+            $this->getUser($result->getStatusCode());
+
         } else {
             $template = new TemplateLoader();
             $render = $template->getTemplate('login.html');
@@ -65,6 +39,38 @@ class HomeController
             } else {
                 echo $render->render(['login' => false]);
             }
+        }
+    }
+
+    public function getUser($status){
+        if ($status === 200) {
+            $get_user = new GuzzleHttp\Client();
+            $secret = 'WEFEpcGFWIq!ya7au-t-Krt_JnJKD6_o9K*l';
+            $key = 'L*D3iK5bMWk9hYRirt_h!pP2';
+            $codice_sha1 = sha1(implode(',', ['userid' => trim($_POST['login']), 'also_check_as_email' => true]) . ',' . $secret);
+            $codice = base64_encode($key . ':' . $codice_sha1);
+            $user = $get_user->post('http://geekhub.docebosaas.com/api/user/checkUsername', [
+                'form_params' => ['userid' => $_POST['login'], 'also_check_as_email' => true],
+                'headers' => [
+                    'X-Authorization' => $codice
+                ]
+            ]);
+            $json = json_decode($user->getBody()->getContents());
+            $_SESSION['docebo_name'] = $json->firstname;
+            $_SESSION['docebo_last'] = $json->lastname;
+            $_SESSION['docebo_email'] = $json->email;
+            $template = new TemplateLoader();
+            $render = $template->getTemplate('login.html');
+            $today = \Carbon\Carbon::now();
+            echo $render->render([
+                'login' => true,
+                'firstname' => $_SESSION['docebo_name'],
+                'lastname' => $_SESSION['docebo_last'],
+                'email' => $_SESSION['docebo_email'],
+                'today' => $today
+            ]);
+        } else {
+            header("Location: /home");
         }
     }
 
